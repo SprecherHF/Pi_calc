acf2e47282698426a731aebfda1825fb8f39fc6a
SprecherHF <100618111+SprecherHF@users.noreply.github.com>
2022-04-19 14:50:31 +0200

    Update main.c

--- U_PiCalc/main.c
+++ U_PiCalc/main.c
@@ -1,7 +1,7 @@
 /*
  * U_PiCalc_FS2022
  *
- * Created: 20.03.2018 18:32:07
+ * Created: 19.04.2022
  * Author : Andreas Sprecher
  */ 
 
@@ -51,6 +51,8 @@ void vButtonTask(void *pvParameters);
 #define PICALC_RESET_ALGO1				1 << 2
 #define PICALC_RESET_ALGO2				1 << 3
 #define PICALC_COMPARE					1 << 4
+#define PICALC_BUTTON3_SHORT			1 << 5
+#define PICALC_BUTTON3_LONG				1 << 6
 
 EventGroupHandle_t Picalculating;
 SemaphoreHandle_t ubergabe;
@@ -69,24 +71,24 @@ int main(void)
 	xEventGroupSetBits(Picalculating, PICALC_ALGO2);
 	
 	
-	xTaskCreate( controllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);
+	xTaskCreate( controllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 2, NULL);
 	xTaskCreate( leibniz_task, (const char *) "Leibniz_PI", configMINIMAL_STACK_SIZE+150, NULL, 1,NULL);
 	xTaskCreate( euler_task, (const char *) "Euler_PI", configMINIMAL_STACK_SIZE+150, NULL, 1,NULL);
 	xTaskCreate( compare_Pi, (const char *) "compare_PI", configMINIMAL_STACK_SIZE+150, NULL, 1,NULL);
-	xTaskCreate(vButtonTask, (const char *) "buttonTask", configMINIMAL_STACK_SIZE+10, NULL, 3, NULL);
+	xTaskCreate( vButtonTask, (const char *) "buttonTask", configMINIMAL_STACK_SIZE+10, NULL, 2, NULL);
 	
 	vDisplayClear();
 	vTaskStartScheduler();
-	
+	char pistring[] = "teststring";
 	vDisplayWriteStringAtPos(0,0,"%s", pistring);
 	
 	return 0;
 }
-
+#define MENU_MAINSCREEN 1
 void controllerTask(void* pvParameters) {
 	(void) pvParameters;
-	static MENU_MAINSCREEN;
-	static menuMode;
+	
+	static uint32_t menuMode = 0;
 	char pistring[12];
 	uint16_t buttonstate = 0x0000;
 	PORTE.DIRSET = 0x08;
@@ -98,11 +100,37 @@ void controllerTask(void* pvParameters) {
 			PORTE.OUTCLR = 0x08;
 			buttonstate = xEventGroupGetBits(Picalculating);
 			vDisplayClear();
-			pistring = printvariable;
-			sprintf(&pistring[0], "PI: %.8f", M_PI);
+			//pistring = printvariable;
+			sprintf(&pistring, "%i", printvariable );
+			//sprintf(&pistring[0], "PI: %.8f", M_PI);
 			vDisplayWriteStringAtPos(1,0, "%s", pistring);
 			switch(menuMode) {
-					case MENU_MAINSCREEN
+					case MENU_MAINSCREEN:
+						if(buttonstate & PICALC_ALGO1) {
+							
+						}
+						if(buttonstate & PICALC_ALGO2) {
+							
+						}
+						if(buttonstate & PICALC_COMPARE) {
+							
+						}
+						//if((xEventGroupGetBits(Picalculating) & PICALC_RESET_ALGO1) == PICALC_RESET_ALGO1) {
+							//xEventGroupClearBits(Picalculating, PICALC_RESET_ALGO1);
+							//pileibniz = 0;
+						{
+							
+						}
+						//if((xEventGroupGetBits(Picalculating) & PICALC_RESET_ALGO2) == PICALC_RESET_ALGO2) {
+							//xEventGroupClearBits(Picalculating, PICALC_RESET_ALGO2);
+							//pieuler = 0;
+						{
+							
+						}
+					break;
+			}
+	}
+}
 	
 
 
@@ -121,16 +149,16 @@ void leibniz_task(void* pvParameters) {
 			xEventGroupClearBits(Picalculating, PICALC_RESET_ALGO1);
 			pileibniz = 0;
 		}
-		if(xSemaphoreTake(uebergabe, 10/portTICK_RATE_MS) == pdTRUE) {
+		if(xSemaphoreTake(ubergabe , 10/portTICK_RATE_MS) == pdTRUE) {
 			uebergabevariable = pileibniz;
-			xSemaphoreGive(uebergabe);
+			xSemaphoreGive(ubergabe );
 		}
 		vTaskDelay(10/portTICK_RATE_MS);
 		
 		for(;;) {
-			if(xSemaphoreTake(uebergabe, 10/portTICK_RATE_MS) == pdTRUE) {
+			if(xSemaphoreTake(ubergabe , 10/portTICK_RATE_MS) == pdTRUE) {
 				printvariable = uebergabevariable;
-				xSemaphoreGive(uebergabe);
+				xSemaphoreGive(ubergabe );
 			}
 			vDisplayClear();
 			vDisplayWriteStringAtPos(0,0,"Leibniz PI: %d", printvariable);
@@ -153,33 +181,35 @@ void euler_task(void* pvParameters) {
 	
 		if((xEventGroupGetBits(Picalculating) & PICALC_RESET_ALGO2) == PICALC_RESET_ALGO2) {
 			xEventGroupClearBits(Picalculating, PICALC_RESET_ALGO2);
-			pileibniz = 0;
+			pieuler = 0;
 			}
-		if(xSemaphoreTake(uebergabe, 10/portTICK_RATE_MS) == pdTRUE) {
-			uebergabevariable = pileibniz;
-			xSemaphoreGive(uebergabe);
+		if(xSemaphoreTake(ubergabe , 10/portTICK_RATE_MS) == pdTRUE) {
+			uebergabevariable = pieuler;
+			xSemaphoreGive(ubergabe );
 			}
 			vTaskDelay(10/portTICK_RATE_MS);
 			
 			for(;;) {
-				if(xSemaphoreTake(uebergabe, 10/portTICK_RATE_MS) == pdTRUE) {
+				if(xSemaphoreTake(ubergabe , 10/portTICK_RATE_MS) == pdTRUE) {
 					printvariable = uebergabevariable;
-					xSemaphoreGive(uebergabe);
+					xSemaphoreGive(ubergabe);
 				}
 				vDisplayClear();
-				vDisplayWriteStringAtPos(0,0,"Leibniz PI: %d", printvariable);
+				vDisplayWriteStringAtPos(0,0,"Euler PI: %d", printvariable);
 				vTaskDelay(200/portTICK_RATE_MS);
 			}			
 	}
 }	
 
 
-// Der Compare Task  startet einfach im angewählten Algorythmus
+// Der Compare task ist komplett in Überarbeitung, komplette Elemente ergeben deshalb keinen erkennbaren Sinn
 
 void compare_Pi(void* pvParameters) {
 	uint32_t comparestart = 1;
 	uint32_t comparestop = 1;
 	uint32_t pifuenftestelle = 3.14159;
+	uint32_t zaehler = 1;
+	uint32_t x = 0;
 	for(;;) {
 		zaehler = pow(x,2);
 		pisqr = pisqr + (1.0 / zaehler);
@@ -203,7 +233,7 @@ void vButtonTask(void * pvParameters) {
 				xEventGroupSetBits(Picalculating, PICALC_BUTTON3_SHORT);
 			}
 			if(getButtonPress(BUTTON4) == SHORT_PRESSED) {
-				xEventGroupSetBits(Picalculating, PICALC_COMPARE;
+				xEventGroupSetBits(Picalculating, PICALC_COMPARE);
 			}
 			if(getButtonPress(BUTTON1) == LONG_PRESSED) {
 				xEventGroupSetBits(Picalculating, PICALC_RESET_ALGO1);
@@ -215,18 +245,20 @@ void vButtonTask(void * pvParameters) {
 				xEventGroupSetBits(Picalculating, PICALC_BUTTON3_LONG);
 			}
 			if(getButtonPress(BUTTON3) == LONG_PRESSED) {
-				uint8_t algostatus = (xEventGroupGetBits(meineEventGroup) & 0x0300 ) >> 8;
+				uint8_t algostatus = (xEventGroupGetBits(Picalculating) & 0x0300 ) >> 8;
 				if(algostatus == 0x01) {
-					xEventGroupClearBits(meineEventGroup, ALGO1);
-					xEventGroupSetBits(meineEventGroup, ALGO2);
+					xEventGroupClearBits(Picalculating, PICALC_ALGO1 );
+					xEventGroupSetBits(Picalculating, PICALC_ALGO2 );
 					} else {
-					xEventGroupClearBits(meineEventGroup, ALGO2);
-					xEventGroupSetBits(meineEventGroup, ALGO1);
+					xEventGroupClearBits(Picalculating, PICALC_ALGO2 );
+					xEventGroupSetBits(Picalculating, PICALC_ALGO1 );
 				}
 			}
 			vTaskDelay(10/portTICK_RATE_MS);
 		}
 	}
+
+	
 	
 	
 	
